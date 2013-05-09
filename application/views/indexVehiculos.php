e36c26f80477236f4cc8282c7a5cbc2b8c2eac67
Andres Mauricio Escobar Pulgarin <anmaescobar@utp.edu.co>
2013-05-08 23:21:36 -0500

    INDEXVEHICULOS.PHP
    YA ESTA HECHO

--- application/views/indexVehiculos.php
+++ application/views/indexVehiculos.php
@@ -1,11 +1,11 @@
 <ul>
-    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/crearVehiculo">Crear vehiculo</a></li>
-    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/modificarVehiculo">Modificar vehiculo</a></li>
-    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/eliminarVehiculo">Borrar vehiculo</a></li>
-    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/vehiculosDisponibles">Vehiculos Disponibles</a></li>
-    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/vehiculosReservados">Vehiculos Reservados</a></li>
-    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/vehiculosAlquilados">Vehiculos Alquilados</a></li>
-    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/vehiculosPorAtributos">Según Atributos del Vehiculo</a></li>
+    <br /> <br /> <li><a href="<?= base_url() ?>index.php/gestionVehiculos/crearVehiculo">Crear vehiculo</a></li> <br /> 
+    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/modificarVehiculo">Modificar vehiculo</a></li><br /> 
+    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/eliminarVehiculo">Borrar vehiculo</a></li> <br /> 
+    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/vehiculosDisponibles">Vehiculos Disponibles</a></li> <br /> 
+    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/vehiculosReservados">Vehiculos Reservados</a></li> <br /> 
+    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/vehiculosAlquilados">Vehiculos Alquilados</a></li> <br /> 
+    <li><a href="<?= base_url() ?>index.php/gestionVehiculos/vehiculosPorAtributos">Según Atributos del Vehiculo</a></li> <br /> 
 </ul>
 <?php
 echo validation_errors();
